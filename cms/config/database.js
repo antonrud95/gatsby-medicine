6408c1e0fdb27b5ed72f1fb1d0d26c5429c746b1
igorbezsmertnyi <mr.igor96@outlook.com>
2020-06-17 21:08:18 +0200

    Feature/setup local env

--- cms/config/database.js
+++ cms/config/database.js
@@ -1,18 +1,45 @@
-module.exports = ({ env }) => ({
-  defaultConnection: 'default',
-  connections: {
-    default: {
-      connector: 'bookshelf',
-      settings: {
-        client: 'postgres',
-        host: env('DATABASE_HOST', '127.0.0.1'),
-        port: env.int('DATABASE_PORT', 5432),
-        database: env('DATABASE_NAME', 'postgres'),
-        username: env('DATABASE_USERNAME', 'postgres'),
-        password: env('DATABASE_PASSWORD', 'postgres'),
-        ssl: env.bool('DATABASE_SSL', false),
+const url = require('url');
+
+module.exports = ({ env }) => {
+  let settings = {};
+
+  if (process.env.NODE_ENV === 'production' && process.env.DATABASE_URL) {
+    const parsed = url.parse(env('DATABASE_URL', ''));
+    const [username, password] = parsed.auth.split(':');
+    const database = parsed.pathname.substr(1);
+
+    console.log(process.env.DATABASE_URL)
+
+    settings = {
+      host: parsed.hostname,
+      port: Number(parsed.port),
+      database,
+      username,
+      password,
+      ssl: (parsed.query.ssl === 'true'),
+    }
+  } else {
+    settings = {
+      host: env('DATABASE_HOST', '127.0.0.1'),
+      port: env.int('DATABASE_PORT', 5432),
+      database: env('DATABASE_NAME', 'postgres'),
+      username: env('DATABASE_USERNAME', 'postgres'),
+      password: env('DATABASE_PASSWORD', 'postgres'),
+      ssl: env.bool('DATABASE_SSL', false),
+    }
+  }
+
+  return {
+    defaultConnection: 'default',
+    connections: {
+      default: {
+        connector: 'bookshelf',
+        settings: {
+          client: 'postgres',
+          ...settings
+        },
+        options: {}
       },
-      options: {}
     },
-  },
-});
+  };
+};
---
+++ frontend/gatsby-config.js
@@ -0,0 +1,46 @@
+require("dotenv").config()
+
+const { CMS_URL } = process.env
+
+module.exports = {
+  siteMetadata: {
+    title: `Gatsby Default Starter`,
+    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    author: `@gatsbyjs`,
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    {
+      resolve: `gatsby-source-strapi`,
+      options: {
+        apiURL: CMS_URL,
+        contentTypes: [
+          // Insert list of resources
+        ],
+        queryLimit: 1000,
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `gatsby-starter-default`,
+        short_name: `starter`,
+        start_url: `/`,
+        background_color: `#663399`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    `gatsby-plugin-offline`,
+  ],
+}
