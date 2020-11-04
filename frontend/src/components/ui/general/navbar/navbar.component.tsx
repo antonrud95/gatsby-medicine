48453c62c6a25dc0dc224f24f80031815f72378d
arud95 <antonrud95@yandex.ru>
2020-11-04 12:26:35 +0200

    feature: add hero navbar

--- frontend/src/components/ui/general/navbar/navbar.component.tsx
+++ frontend/src/components/ui/general/navbar/navbar.component.tsx
@@ -1,51 +1,33 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { Container, Navbar, Nav } from 'react-bootstrap'
 import { Link } from 'gatsby'
 import classnames from 'classnames'
 
-import Logo from '~/components/ui/general/logo/logo.component'
-import MobileMenu from '~/components/ui/general/mobile-menu/mobile-menu.component'
 import Button from '~/components/ui/general/button/button.component'
-
-import DarkMenuOpenIcon from '~/assets/icons/menu-open-icon-dark.svg'
 
 import styles from './navbar.module.scss'
 
 const SNavbar = () => {
-  const [isMenuShown, toggleMenu] = useState(false)
-
-  const openClickHandler = () => {
-    toggleMenu(true)
-  }
-
   return (
     <Navbar
       fixed="top"
       className={classnames(styles.navbar, styles.navbar__onLight)}
     >
       <Container>
-        <Logo />
-        <DarkMenuOpenIcon
-          className={styles.openIcon}
-          onClick={openClickHandler}
-        />
-
+        <Link to="/" className={styles.navLinkLogo}>
+          Logo
+        </Link>
         <Nav className={classnames(styles.nav, 'mr-4', 'ml-auto')}>
           <Link to="/" className={styles.navLink}>
-            Nav Item #1
-          </Link>
-          <Link to="/" className={styles.navLink}>
-            Nav Item #2
-          </Link>
-          <Link to="/" className={styles.navLink}>
-            Nav Item #3
+            +38 050 937 99 92
           </Link>
         </Nav>
         <Nav className={styles.nav}>
-          <Button variant={'primary'}>Button</Button>
+          <Button variant={'secondary'} className={styles.navBtn}>
+            Корзина
+          </Button>
         </Nav>
       </Container>
-      <MobileMenu isShown={isMenuShown} toggle={toggleMenu} />
     </Navbar>
   )
 }
