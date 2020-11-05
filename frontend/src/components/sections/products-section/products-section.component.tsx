14277ad46c4d83ca5ad6c44dc9dc7ec305b80ae0
arud95 <antonrud95@yandex.ru>
2020-11-05 13:16:21 +0200

    refactor: clean production section styles

--- frontend/src/components/sections/products-section/products-section.component.tsx
+++ frontend/src/components/sections/products-section/products-section.component.tsx
@@ -13,36 +13,39 @@ interface Props {
 
 const ProductsSection: FC<Props> = ({ sales }) => {
   return (
-    <Container>
-      {sales.map((sale: SalesType, index) => {
-        return (
-          <SaleItem
-            key={sale.id}
-            title={sale.title}
-            containerDescription={sale.descFirst}
-            containerDescriptionSecond={sale.descSecond}
-            containerDescriptionThird={sale.descThird}
-            containerDescriptionFourth={sale.descFourth}
-            price={sale.price}
-            oldPrice={sale.oldPrice}
-            image={sale.image.childImageSharp.fluid}
-            imageStyles={styles.imgItemStyles}
-            children={'Добавить в корзину'}
-            variant={'primary'}
-            itemWrapper={styles.productItem}
-            titleStyles={styles.productTitle}
-            contentContainer={styles.rowDescriptionContainer}
-            contentContainerItem={styles.rowDescription}
-            priceContainer={styles.priceContainer}
-            containerSpan={styles.productSpan}
-            containerTextStyles={styles.rowText}
-            containerDescriptionStyles={styles.rowBlueText}
-            stylesPrice={styles.price}
-            stylesOldPrice={styles.oldPrice}
-            btnStyles={styles.btnStyles}
-          />
-        )
-      })}
+    <Container fluid>
+      <h2 className={styles.mainTitle}>Акционные товары</h2>
+      <Container className={styles.productsContainer}>
+        {sales.map((sale: SalesType, index) => {
+          return (
+            <SaleItem
+              key={sale.id}
+              title={sale.title}
+              containerDescription={sale.descFirst}
+              containerDescriptionSecond={sale.descSecond}
+              containerDescriptionThird={sale.descThird}
+              containerDescriptionFourth={sale.descFourth}
+              price={sale.price}
+              oldPrice={sale.oldPrice}
+              image={sale.image.childImageSharp.fluid}
+              imageStyles={styles.imgItemStyles}
+              children={'Добавить в корзину'}
+              variant={'primary'}
+              itemWrapper={styles.productItem}
+              titleStyles={styles.productTitle}
+              contentContainer={styles.rowDescriptionContainer}
+              contentContainerItem={styles.rowDescription}
+              priceContainer={styles.priceContainer}
+              containerSpan={styles.productSpan}
+              containerTextStyles={styles.rowText}
+              containerDescriptionStyles={styles.rowBlueText}
+              stylesPrice={styles.price}
+              stylesOldPrice={styles.oldPrice}
+              btnStyles={styles.btnStyles}
+            />
+          )
+        })}
+      </Container>
     </Container>
   )
 }
